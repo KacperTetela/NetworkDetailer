037a59f375d9eb66b96b3468924065306cb9ae1c
KacperTetela <kacpertetela1@gmail.com>
2024-09-01 18:46:57 +0200

    refactor hardware features

--- src/main/java/networkdetailer/com/model/hardware/CPUInfo.java
+++
@@ -1,4 +0,0 @@
-package networkdetailer.com.model.hardware;
-
-public record CPUInfo(CPUManufacturer cpuManufacturer, int generation) {
-}
---
+++ src/main/java/networkdetailer/com/model/hardware/MOBOData.java
@@ -0,0 +1,4 @@
+package networkdetailer.com.model.hardware;
+
+public record MOBOData(String model, String manufacturer, String bios) {
+}
